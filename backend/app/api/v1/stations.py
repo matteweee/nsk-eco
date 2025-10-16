df3eaa761cccd7aab033c510341900e4e7adf39d
matvey nikhaev <mdillplay@gmail.com>
2025-10-16 07:07:27 +0700

    new plot names

--- backend/app/api/v1/stations.py
+++ backend/app/api/v1/stations.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 
 import matplotlib
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 from app.station_data import stations
 from app.schedules import time_zone_schedule, clustering_schedule
@@ -49,8 +50,8 @@ async def get_plot(option: int):
         fig, ax = plt.subplots(figsize=(5, 4))
         cluster_zones_no_heads = clustering_schedule(stations, 10, ax, mode='battery_life')
 
-    filename = f"plot_{option}.png"
-    plt.savefig(filename)
+    filename = f"app/static/plots/plot_{option}_{datetime.now().strftime('%d.%m.%Y_%H:%M:%S')}.png"
+    plt.savefig(filename, bbox_inches='tight')
     plt.close()
-
+    
     return FileResponse(filename, media_type="image/png")
